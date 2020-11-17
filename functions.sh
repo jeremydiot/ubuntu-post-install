24148e186ea39d69ee3f4adb9d6e440ba32d77a1
Jeremy DIOT <diot.jeremy@outlook.com>
2020-11-17 21:10:52 +0000

    uncomment

--- functions.sh
+++ functions.sh
@@ -6,28 +6,28 @@ function echo_action(){
 
 function apt_update(){
     echo_action "APT-UPDATE"
-    # apt-get -y update
+    apt-get -y update
 }
 
 function apt_upgrade(){
     echo_action "APT-UPGRADE"
-	# apt-get -y upgrade
-	# apt-get -y dist-upgrade
+	apt-get -y upgrade
+	apt-get -y dist-upgrade
 }
 
 function apt_clean(){
     echo_action "APT-CLEAN"
-    # apt-get -y autoremove
-	# apt-get -y clean
+    apt-get -y autoremove
+	apt-get -y clean
 }
 
 function apt_install(){
     echo_action "APT-INSTALL"$1
-    # apt-get -y install $1
+    apt-get -y install $1
 }
 
 function apt_repository(){
     echo_action "ADD-APT-REPOSITORY"
-    # add-apt-repository $1
+    add-apt-repository $1
 }
 
